c155518c3402183eb2d880322964a6f7ffb7844a
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-06-28 11:56:49 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,112 +5,120 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void decimal_to_baseN(char *converted, unsigned int num, int base) 
-{
-    if (num == 0) {
-        converted[0] = '0';
-        converted[1] = '\0';
-        return;
-    }
-    int MAX_REP_LEN = 250;
-    char NUM_BASE[] = "0123456789ABCDEF";
-    char buffer[MAX_REP_LEN];
+int print_number(int n)
+{int x, k, r = 1, s = 0;
 
-    int i = MAX_REP_LEN - 1;
-    while (num != 0) {
-        buffer[i--] = NUM_BASE[num % base];
-        num /= base;
-    }
-    int counter = 0;
-    for (int j = i + 1; j <= MAX_REP_LEN - 1; j++) {
-        converted[counter++] = buffer[j];
-    }
-    converted[counter] = '\0';
+	if (n == 0)
+	{putchar(48);
+		s += 1;
+		return (s);
+	}
+	x = n;
+	if (x == -2147483648)
+	{x = x + 1;
+	}
+	if (n < 0)
+	{x = -x;
+		putchar('-');
+		s += 1;
+	}
+	while (r < 1000000000 && (x - (r * 10) >= 0))
+	{r = r * 10;
+	}
+	while (x > 0)
+	{k = x / r;
+		x = x % r;
+		r /= 10;
+		if (x < r)
+		{putchar(k + 48);
+			 s += 1;
+			putchar(48);
+			 s += 1;
+			r /= 10;
+		}
+		else if (n == -2147483648 && k == 7 && r == 0)
+		{putchar(k + 49);
+			 s += 1;
+		}
+		else
+		{putchar(k + 48);
+			 s += 1;
+		}
+	}
+	return (s);
 }
-
-
-void my_print(char *c, ...)
+/**
+ *pr_char - pr
+ *@c: char
+ *Return: char
+ */
+int pr_char(char c)
 {
-    int num;
-    va_list arg;
-    va_start(arg, c);
-    char outbuf[2048];
-    int i;
-    char *ch;
-    double db_val;
-    unsigned int uint_val;
-    while(*c!='\0'){
-        if(*c=='%'){
-            c++; // To incement to the formating character
-            // processing the formatting character
-            switch(*c){
-                case 'b':
-                case 'B':
-                case 'h':
-                case 'H':
-                case 'O':
-                case 'o':
-                case 'd':
-                case 'D':
-                case 'i':
-                case 'I':
-                    num = va_arg(arg, int);
-                    if(num<0){
-                        // Simple - sign is used instead of 2s complement
-                        local_put('-');
-                        num = num * -1;
-                    }
-                    if(*c=='b' || *c=='B'){
-                        decimal_to_baseN(outbuf, num, 2);
-                    }else if(*c=='o' || *c=='O'){
-                        decimal_to_baseN(outbuf, num, 8);
-                    }else if(*c=='d' || *c=='D'){
-                        decimal_to_baseN(outbuf, num, 10);
-                    }else if(*c=='h' || *c=='H'){
-                        decimal_to_baseN(outbuf, num, 16);
-                    }
-                    
-                    for(int i=0;outbuf[i]!='\0';i++){
-                        local_put(outbuf[i]);
-                    }
-                break;
-                
-                case 'c':
-                case 'C':
-                    num = va_arg(arg, int);
-                    local_put(num);
-                break;
-                    
-                case 's':
-                case 'S':
-                    ch = va_arg(arg, char*);
-                    while(*ch!='\0'){
-                        local_put(*ch++);
-                    }
-                break;
-                
-                case 'f':
-                case 'F':
-                    db_val = va_arg(arg, double);
-                    sprintf(outbuf, "%f", db_val);
-                    for(int i=0;outbuf[i]!='\0';i++){
-                        local_put(outbuf[i]);
-                    }
-                break;
-                
-                case 'u':
-                case 'U':
-                    uint_val = va_arg(arg, unsigned int);
-                    sprintf(outbuf, "%u", uint_val);
-                    for(int i=0;outbuf[i]!='\0';i++){
-                        local_put(outbuf[i]);
-                    }
-                break;
-            }
-        }else{
-            local_put(*c);   
-        }
-        c++;
-    }
-    va_end(arg);
+	putchar(c);
+	return (1);
 }
+/**
+ *pr_string - prs
+ *@ch: str
+ *Return: string
+ */
+int pr_string(char *ch)
+{
+	int i;
+
+	if (ch == NULL)
+		return (0);
+	for (i = 0; ch[i] != '\0'; i++)
+		putchar(ch[i]);
+	return (i);
+}
+/**
+ *_printf - print any thing
+ *@format: input
+ *Return: int
+ */
+int _printf(const char *format, ...)
+{int i = 0, j, length = 0;
+	char c, *ch;
+	va_list l;
+
+	if (format == NULL)
+		return (-1);
+	va_start(l, format);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{putchar(format[i]);
+			length += 1;
+		}
+		else
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+			if (format[i + 1] == 'd')
+			{j = va_arg(l, int);
+				length += print_number(j);
+			}
+			if (format[i + 1] == 'c')
+			{c = va_arg(l, int);
+				length += pr_char(c);
+			}
+			if (format[i + 1] == 's')
+			{ch = va_arg(l, char*);
+				if (!ch)
+					ch = "(null)";
+				length += pr_string(ch);
+			}
+			if (format[i + 1] == 'i')
+			{j =  va_arg(l, int);
+				length += print_number(j);
+			}
+			if (format[i + 1] == '%')
+			{putchar('%');
+				length += 1;
+			}
+			i++;
+		}
+		i++;
+	va_end(l); }
+	return (length); }
