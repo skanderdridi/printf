0f55f3f3cc98ff7e423f38f4417273c2f9ea9844
Skander <62188738+skanderdridi@users.noreply.github.com>
2023-06-28 11:41:01 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,282 +5,169 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int print_number(int n)
-{int x, k, r = 1, s = 0;
-
-	if (n == 0)
-	{putchar(48);
-		s += 1;
-		return (s);
-	}
-	x = n;
-	if (x == -2147483648)
-	{x = x + 1;
-	}
-	if (n < 0)
-	{x = -x;
-		putchar('-');
-		s += 1;
-	}
-	while (r < 1000000000 && (x - (r * 10) >= 0))
-	{r = r * 10;
-	}
-	while (x > 0)
-	{k = x / r;
-		x = x % r;
-		r /= 10;
-		if (x < r)
-		{putchar(k + 48);
-			 s += 1;
-			putchar(48);
-			 s += 1;
-			r /= 10;
-		}
-		else if (n == -2147483648 && k == 7 && r == 0)
-		{putchar(k + 49);
-			 s += 1;
-		}
-		else
-		{putchar(k + 48);
-			 s += 1;
-		}
-	}
-	return (s);
-}
-/**
- *pr_char - pr
- *@c: char
- *Return: char
- */
-int pr_char(char c)
+static void printchar(char **str, int c)
 {
-	putchar(c);
-	return (1);
+	extern int putchar(int c);
+	if (str) {
+		**str = c;
+		++(*str);
+	}
+	else (void)putchar(c);
 }
-/**
- *pr_string - prs
- *@ch: str
- *Return: string
- */
-int pr_string(char *ch)
+
+#define PAD_RIGHT 1
+#define PAD_ZERO 2
+
+static int prints(char **out, const char *string, int width, int pad)
 {
-	int i;
+	register int pc = 0, padchar = ' ';
 
-	if (ch == NULL)
-		return (0);
-	for (i = 0; ch[i] != '\0'; i++)
-		putchar(ch[i]);
-	return (i);
-}
-/**
- *_printf - print any thing
- *@format: input
- *Return: int
- */
-int _printf(const char *format, ...)
-{int i = 0, j, length = 0;
-	char c, *ch;
-	va_list l;
-
-	if (format == NULL)
-		return (-1);
-	va_start(l, format);
-	while (format[i])
-	{
-		if (format[i] != '%')
-		{putchar(format[i]);
-			length += 1;
+	if (width > 0) {
+		register int len = 0;
+		register const char *ptr;
+		for (ptr = string; *ptr; ++ptr) ++len;
+		if (len >= width) width = 0;
+		else width -= len;
+		if (pad & PAD_ZERO) padchar = '0';
+	}
+	if (!(pad & PAD_RIGHT)) {
+		for ( ; width > 0; --width) {
+			printchar (out, padchar);
+			++pc;
 		}
-		else
-		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			if (format[i + 1] == 'd')
-			{j = va_arg(l, int);
-				length += print_number(j);
+	}
+	for ( ; *string ; ++string) {
+		printchar (out, *string);
+		++pc;
+	}
+	for ( ; width > 0; --width) {
+		printchar (out, padchar);
+		++pc;
+	}
+
+	return pc;
+}
+
+/* the following should be enough for 32 bit int */
+#define PRINT_BUF_LEN 12
+
+static int printi(char **out, int i, int b, int sg, int width, int pad, int letbase)
+{
+	char print_buf[PRINT_BUF_LEN];
+	register char *s;
+	register int t, neg = 0, pc = 0;
+	register unsigned int u = i;
+
+	if (i == 0) {
+		print_buf[0] = '0';
+		print_buf[1] = '\0';
+		return prints (out, print_buf, width, pad);
+	}
+
+	if (sg && b == 10 && i < 0) {
+		neg = 1;
+		u = -i;
+	}
+
+	s = print_buf + PRINT_BUF_LEN-1;
+	*s = '\0';
+
+	while (u) {
+		t = u % b;
+		if( t >= 10 )
+			t += letbase - '0' - 10;
+		*--s = t + '0';
+		u /= b;
+	}
+
+	if (neg) {
+		if( width && (pad & PAD_ZERO) ) {
+			printchar (out, '-');
+			++pc;
+			--width;
+		}
+		else {
+			*--s = '-';
+		}
+	}
+
+	return pc + prints (out, s, width, pad);
+}
+
+static int print(char **out, int *varg)
+{
+	register int width, pad;
+	register int pc = 0;
+	register char *format = (char *)(*varg++);
+	char scr[2];
+
+	for (; *format != 0; ++format) {
+		if (*format == '%') {
+			++format;
+			width = pad = 0;
+			if (*format == '\0') break;
+			if (*format == '%') goto out;
+			if (*format == '-') {
+				++format;
+				pad = PAD_RIGHT;
 			}
-			if (format[i + 1] == 'c')
-			{c = va_arg(l, int);
-				length += pr_char(c);
+			while (*format == '0') {
+				++format;
+				pad |= PAD_ZERO;
 			}
-			if (format[i + 1] == 's')
-			{ch = va_arg(l, char*);
-				if (!ch)
-					ch = "(null)";
-				length += pr_string(ch);
+			for ( ; *format >= '0' && *format <= '9'; ++format) {
+				width *= 10;
+				width += *format - '0';
 			}
-			if (format[i + 1] == 'i')
-			{j =  va_arg(l, int);
-				length += print_number(j);
+			if( *format == 's' ) {
+				register char *s = *((char **)varg++);
+				pc += prints (out, s?s:"(null)", width, pad);
+				continue;
 			}
-			if (format[i + 1] == '%')
-			{putchar('%');
-				length += 1;
+			if( *format == 'd' ) {
+				pc += printi (out, *varg++, 10, 1, width, pad, 'a');
+				continue;
 			}
-			i++;
+			if( *format == 'x' ) {
+				pc += printi (out, *varg++, 16, 0, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'X' ) {
+				pc += printi (out, *varg++, 16, 0, width, pad, 'A');
+				continue;
+			}
+			if( *format == 'u' ) {
+				pc += printi (out, *varg++, 10, 0, width, pad, 'a');
+				continue;
+			}
+			if( *format == 'c' ) {
+				/* char are converted to int then pushed on the stack */
+				scr[0] = *varg++;
+				scr[1] = '\0';
+				pc += prints (out, scr, width, pad);
+				continue;
+			}
 		}
-		i++;
-	va_end(l); }
-	return (length); }
-int puns(va_list arg)
-{
-	unsigned int num, a = 0, div;
-
-	num = va_arg(arg, int);
-
-	if (num == 0)
-	{
-		putchar('0');
-		a = 1;
-	}
-	if (num > 0)
-	{
-		for (div = 1; (num / div) > 9; div *= 10)
-			;
-		while (div != 0)
-		{
-			putchar((num / div) + '0');
-			num %= div;
-			div /= 10;
-			a++;
+		else {
+		out:
+			printchar (out, *format);
+			++pc;
 		}
 	}
-	return (a);
+	if (out) **out = '\0';
+	return pc;
 }
 
-/**
- * poct - Unsigned octal
- *
- * @arg: arg
- * Return: length
- */
-int poct(va_list arg)
+/* assuming sizeof(void *) == sizeof(int) */
+
+int printf(const char *format, ...)
 {
-
-	unsigned int num, a = 0, i = 0;
-	int tab[100];
-
-	num = va_arg(arg, int);
-	if (num < 9)
-	{
-		putchar(num + '0');
-		a = 1;
-	}
-	else if (num >= 9)
-	{
-		while (num > 0)
-		{
-			tab[i] = num % 8;
-			num /= 8;
-			i++;
-		}
-	}
-	while (i--)
-	{
-		putchar(tab[i] + '0');
-		a++;
-	}
-	return (a);
+	register int *varg = (int *)(&format);
+	return print(0, varg);
 }
 
-/**
- * phex - Unsigned hexadecimal integer
- * @arg: arg
- * Return: length
- */
-int phex(va_list arg)
+int sprintf(char *out, const char *format, ...)
 {
-	unsigned int n = va_arg(arg, unsigned int);
-	unsigned int a[1024];
-	int i = 0, t = 0;
-	char p;
-
-	if (n < 1)
-	{
-		write(1, "0", 1);
-		return (1);
-	}
-	for (i = 0; n > 0; i++, t++)
-	{
-		a[t] = n % 16;
-		n = n / 16;
-		if (a[t] > 10)
-			a[i] = a[t] + 39;
-		else
-			a[i] = a[t];
-	}
-	for (i = t - 1; i >= 0; i--)
-	{
-		p = a[i] + '0';
-		putchar(p);
-	}
-	return (t);
-}
-
-/**
- * pHEX - Unsigned hexadecimal integer (uppercase)
- * @arg: arg
- * Return: length
- */
-int pHEX(va_list arg)
-{
-
-	unsigned int n = va_arg(arg, unsigned int);
-	int a[1024], i = 0, t = 0;
-	char p;
-
-	if (n < 1)
-	{
-		putchar('0');
-		return (1);
-	}
-	for (i = 0; n > 0; i++)
-	{
-		a[t] = n % 16;
-		n = n / 16;
-		if (a[i] > 10)
-			a[i] = a[t] + 7;
-		else
-		{
-			a[i] = a[t];
-		}
-		t++;
-	}
-	for (i = t - 1; i >= 0; i--)
-	{
-		p = a[i] + '0';
-		putchar(p);
-	}
-	return (t);
-}
-
-/**
- * pSTR - return an str
- * @arg: var to write
- * Return: string
- */
-int pSTR(va_list arg)
-{
-	int i = 0, b;
-	char *s = va_arg(arg, char *);
-
-	if (s == NULL)
-		s = "(null)";
-	while (s[i] != '\0')
-	{
-		b = s[i];
-		if (b == 92 && s[i + 1] == 'n')
-		{
-			putchar('x');
-			putchar('0');
-			putchar('A');
-			i++;
-		}
-		else
-		{
-		putchar(s[i]);
-		i++;
-	}
-	}
-	return (i);
+	register int *varg = (int *)(&format);
+	return print(&out, varg);
 }
